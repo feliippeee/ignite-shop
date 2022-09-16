1716a16c9df8ca860d5c0ea85ad462c81c1fc163
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-16 00:26:58 -0300

    Produto & Checkout - Trabalhando com SEO

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next";
 import Image from "next/future/image";
+import Head from "next/head";
 import Link from "next/link";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
@@ -15,19 +16,27 @@ interface SuccessProps {
 
 export default function Success({ customerName, product}: SuccessProps) {
     return (
-        <SuccessContainer>
-            <h1>Compra Efetuada!</h1>
-            <ImageContainer>
-                <Image src={product.imageUrl} width={120} height={110} alt="" />
-            </ImageContainer>
-            <p>
-                Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-            </p>
+        <>
+            <Head>
+                <title>Compra efetuada | Ignite Shop</title>
+            </Head>
 
-            <Link href="/">
-                Voltar ao catálogo
-            </Link>
-        </SuccessContainer>
+            <meta name="robots" content="noIndex" />
+
+            <SuccessContainer>
+                <h1>Compra Efetuada!</h1>
+                <ImageContainer>
+                    <Image src={product.imageUrl} width={120} height={110} alt="" />
+                </ImageContainer>
+                <p>
+                    Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+                </p>
+
+                <Link href="/">
+                    Voltar ao catálogo
+                </Link>
+            </SuccessContainer>
+        </>
     )
 }
 
