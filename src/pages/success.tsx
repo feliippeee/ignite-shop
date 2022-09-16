56168354f28a8e05b2b2b1178893cffdf4281477
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-16 00:08:38 -0300

    Produto & Checkout - Dados da compra no sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,4 +1,7 @@
+import { GetServerSideProps } from "next";
 import Link from "next/link";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
 import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 
 export default function Success() {
@@ -17,4 +20,24 @@ export default function Success() {
             </Link>
         </SuccessContainer>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    const sessionId = String(query.session_id);
+
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+        expand: ['line_items', 'line_items.data.price.product']
+    })
+
+    const customerName = session.customer_details.name;
+    const product = session.line_items.data[0].product as Stripe.Product
+    return { 
+        props: {
+            customerName,
+            product: {
+                name: product.name,
+                imageUrl: product.images[0],
+            }
+        }
+    }
 }
